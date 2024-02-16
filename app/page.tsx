3fc614276b3d382846d87326cf1917ae24b7574f
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-16 11:15:04 -0600

    changed the way website name is displayed

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
     const subdomain = typeof window !== 'undefined' ? getSubdomain(window.location.href) : '';
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl">This page ({subdomain}) does not exist</h1>
+            <h1 className="text-2xl">{subdomain} does not exist</h1>
             <button
                 className="mt-4 px-4 py-2 bg-white text-black rounded hover:shadow-[0_10px_50px_5px_rgba(255,255,255,0.3)] duration-500"
                 onClick={() => window.location.href = "https://www.aaryandehade.me"}>
